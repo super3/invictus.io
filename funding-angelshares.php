2be4668f9185e994af0a75e2c83246a3ce4fec8a
super3 <me@super3.org>
2013-12-29 21:39:17 -0500

    Content Switch

--- funding-angelshares.php
+++ funding-angelshares.php
@@ -27,28 +27,32 @@
 				contribution to the industry infrastructure.
 			</p>
 
-			<h3>Shares in DACs</h3>	
+			<h3>How to acquire Angelshares?</h3>					
 			<p style="text-align: justify;">								
-				Both ProtoShares and AngelShares rely upon the willingness of developers to honor those contributions
-				with shares in the DACs they develop.  We expect a Social Consensus to form in the marketplace about
-				what constitutes honorable developer behavior. ProtoShares and AngelShares constitute concrete 
-				documentation of what benefits developers received from those who laid the industry foundation.  
-				Shares in individual DACs are concrete way for developers to acknowledge and reciprocate those benefits.
-				How the market responds to a new DAC will depend upon how well that DAC is perceived to have honored 
-				the emerging Social Consensus.
+				Beginning New Year’s Day 2014, there will be 10,000 new AngelShares available in a new patron competition, every day, for 200 days. 
+				5000 of them will be available to ProtoShare holders. 5000 of them will be available to Bitcoin holders. That's ultimately 2,000,000 total shares, just like ProtoShares.
 			</p>
-			<p style="text-align: justify;">
-				We see a consensus beginning to form around the following principles: 
-				(a) Developers should seek a reasonable balance between equal-opportunity lotteries and ways to compensate
-				those who sacrifice their savings or labor. (b) The market needs to give developers freedom to raise 
-				funds to develop, support and promote their new DACs.  We believe that the following allocation represents
-				the minimum fair genesis allocation.
+			<p>
+				Those who donate bitcoins to this Angel Address will proportionally split 5000 AngelShares:
 			</p>
-			<ul style="list-style-type: circle;">
-				<li>10% of the crypto-equity should be allocated to holders of ProtoShares.</li>
-				<li>10% of the crypto-equity should be allocated to holders of AngelShares.</li>
-				<li>80% of the crypto-equity should be customized to the needs of each DAC.</li>
-			</ul>
+				<pre>1ANGELwQwWxMmbdaSWhWLqBEtPTkWb8uDc</pre>
+			<p>
+				Those who donate ProtoShares to this Angel Address will proportionally split 5000 AngelShares:
+			</p>
+				<pre>PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw</pre>
+			<p>
+				Every day the shares are divided among those who donated that day.
+			</p>
+			<h3>Links</h3>
+			<p>	
+				<ul style="list-style-type: circle;">
+					<li><a href="#">FAQ - An overview of answers to frequently asked questions</a></li>
+					<li><a href="#">Terms and Conditions - PDF file of the Terms and Conditions for Angelshares</a></li>
+					<li><a href="#">Social Consensus - PDF file of the social contract between Invictus Innovations and investors</a></li>
+				</ul>
+			</p>	
+
+			
 		</div><!-- /col-lg-4 -->
 		
 	
@@ -101,31 +105,29 @@
 				<div class="row">				
 					<div class="col-lg-12">					
 							
-							
-						<h3>How to acquire Angelshares?</h3>					
-							<p style="text-align: justify;">								
-								Beginning New Year’s Day 2014, there will be 10,000 new AngelShares available in a new patron competition, every day, for 200 days. 
-								5000 of them will be available to ProtoShare holders. 5000 of them will be available to Bitcoin holders. That's ultimately 2,000,000 total shares, just like ProtoShares.
-							</p>
-							<p>
-								Those who donate bitcoins to this Angel Address will proportionally split 5000 AngelShares:
-							</p>
-								<pre>1ANGELwQwWxMmbdaSWhWLqBEtPTkWb8uDc</pre>
-							<p>
-								Those who donate ProtoShares to this Angel Address will proportionally split 5000 AngelShares:
-							</p>
-								<pre>PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw</pre>
-							<p>
-								Every day the shares are divided among those who donated that day.
-							</p>
-						<h3>Links</h3>
-							<p>	
-								<ul style="list-style-type: circle;">
-									<li><a href="#">FAQ - An overview of answers to frequently asked questions</a></li>
-									<li><a href="#">Terms and Conditions - PDF file of the Terms and Conditions for Angelshares</a></li>
-									<li><a href="#">Social Consensus - PDF file of the social contract between Invictus Innovations and investors</a></li>
-								</ul>
-							</p>	
+					<h3>Shares in DACs</h3>	
+					<p style="text-align: justify;">								
+					Both ProtoShares and AngelShares rely upon the willingness of developers to honor those contributions
+					with shares in the DACs they develop.  We expect a Social Consensus to form in the marketplace about
+					what constitutes honorable developer behavior. ProtoShares and AngelShares constitute concrete 
+					documentation of what benefits developers received from those who laid the industry foundation.  
+					Shares in individual DACs are concrete way for developers to acknowledge and reciprocate those benefits.
+					How the market responds to a new DAC will depend upon how well that DAC is perceived to have honored 
+					the emerging Social Consensus.
+					</p>
+					<p style="text-align: justify;">
+					We see a consensus beginning to form around the following principles: 
+					(a) Developers should seek a reasonable balance between equal-opportunity lotteries and ways to compensate
+					those who sacrifice their savings or labor. (b) The market needs to give developers freedom to raise 
+					funds to develop, support and promote their new DACs.  We believe that the following allocation represents
+					the minimum fair genesis allocation.
+					</p>
+					<ul style="list-style-type: circle;">
+					<li>10% of the crypto-equity should be allocated to holders of ProtoShares.</li>
+					<li>10% of the crypto-equity should be allocated to holders of AngelShares.</li>
+					<li>80% of the crypto-equity should be customized to the needs of each DAC.</li>
+					</ul>
+					
 					</div>			
 				</div>	
 				<br><br>
