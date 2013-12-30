1c1e42195605a601e77e939fc8f8c6316115baa1
System Administrator <root@Jamess-MacBook-Air.local>
2013-12-30 18:14:44 -0500

    Adding price table and recent price chart

---
+++ getprice.php
@@ -0,0 +1,22 @@
+<?php
+
+if($_REQUEST['exchange'] == 'Bter')
+{
+	$api_val = file_get_contents('https://bter.com/api/1/ticker/pts_btc');	
+	echo $api_val;
+}
+elseif ($_REQUEST['exchange'] == 'Cryptsy')
+{
+	$api_val = file_get_contents('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=119');	
+	echo $api_val;
+}
+elseif ($_REQUEST['exchange'] == 'Btc38')
+{
+
+}
+elseif ($_REQUEST['exchange'] == 'Peatio')
+{
+
+}
+
+?>
