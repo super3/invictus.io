f502f653a0df02951a0cdab7de8f750cccbf2ec9
super3 <me@super3.org>
2013-12-29 20:42:55 -0500

    Trying PHP Files.

---
+++ footer.php
@@ -0,0 +1,98 @@
+	<! ========== CALL TO ACTION BAR =============================================================================================== 
+	=============================================================================================================================>    
+	<div id="cta-bar">
+		<div class="container">
+			<div class="row centered">
+				<a href="#"><h4>Are You Ready For The Next Step?</h4></a>
+			</div>
+		</div><!-- /container -->
+	</div><!-- /cta-bar -->
+	
+	<! ========== FOOTER ======================================================================================================== 
+	=============================================================================================================================>    
+	
+	<div id="f">
+		<div class="container">
+			<div class="row">
+				<!-- ADDRESS -->
+				<div class="col-lg-3">
+					<h4>Our Studio</h4>
+					<p>
+						Invictus Innovations Incorporated<br/>
+						2020 Kraft Dr., Suite 3030<br/>
+						 Blacksburg, VA 24060, USA<br/>
+					</p>
+					<p>
+						<i class="fa fa-mobile"></i> +55 4893.8943<br/>
+						<i class="fa fa-envelope-o"></i> hello@yourdomain.com
+					</p>
+				</div><! --/col-lg-3 -->
+				
+				<!-- LATEST POSTS -->
+				<div class="col-lg-3">
+					<h4>Exchanges</h4>
+					<p>
+						<i class="fa fa-angle-right"></i> A post with an image<br/>
+						<i class="fa fa-angle-right"></i> Other post with a video<br/>
+						<i class="fa fa-angle-right"></i> A full width post<br/>
+						<i class="fa fa-angle-right"></i> We talk about something nice<br/>
+						<i class="fa fa-angle-right"></i> Yet another single post<br/>
+					</p>
+				</div><!-- /col-lg-3 -->
+				
+				<!-- LATEST POSTS -->
+				<div class="col-lg-3">
+					<h4>Mining</h4>
+					<p>
+						<i class="fa fa-angle-right"></i> A post with an image<br/>
+						<i class="fa fa-angle-right"></i> Other post with a video<br/>
+						<i class="fa fa-angle-right"></i> A full width post<br/>
+						<i class="fa fa-angle-right"></i> We talk about something nice<br/>
+						<i class="fa fa-angle-right"></i> Yet another single post<br/>
+					</p>
+				</div><!-- /col-lg-3 -->
+				
+				<!-- LATEST POSTS -->
+				<div class="col-lg-3">
+					<h4>Services</h4>
+					<p>
+						<i class="fa fa-angle-right"></i> A post with an image<br/>
+						<i class="fa fa-angle-right"></i> Other post with a video<br/>
+						<i class="fa fa-angle-right"></i> A full width post<br/>
+						<i class="fa fa-angle-right"></i> We talk about something nice<br/>
+						<i class="fa fa-angle-right"></i> Yet another single post<br/>
+					</p>
+				</div><!-- /col-lg-3 -->
+				
+				
+			</div><! --/row -->
+		</div><!-- /container -->
+	</div><!-- /f -->
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/retina.js"></script>
+	<script>
+		$(window).scroll(function() {
+			$('.si').each(function(){
+			var imagePos = $(this).offset().top;
+	
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+400) {
+					$(this).addClass("slideUp");
+				}
+			});
+		});
+	</script>    
+    <script>
+	    $('#myTab a').click(function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		})
+	</script>  
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Invictus Innovations - Decentralized Solutions</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/colors/color-3498db.css" rel="stylesheet">    
+    <link href="assets/css/animations.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    
+    <!-- JavaScripts needed at the beginning
+    ================================================== -->
+    <script type="text/javascript" src="http://alvarez.is/demo/newfirm/assets/js/twitterFetcher_v10_min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/hover.zoom.js"></script>
+    <script src="assets/js/hover.zoom.conf.js"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
