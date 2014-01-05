aabca8ddbb57855e230c4c797ee0a416a9bd6c82
super3 <me@super3.org>
2014-01-05 18:33:03 -0500

    Removed Text

--- team.php
+++ team.php
@@ -38,81 +38,80 @@
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Bo Shen - Bio Card.png" alt="" /></a>
-				<p>	Bo Shen<br>Director, CEO China </p>
+				<!--<p>	Bo Shen<br>Director, CEO China </p>
 				<p style="text-align: justify;">
 					Bo Shen is an evangelist, entrepreneur and inverter in math based ecosystem. As a veteran of the financial and Internet industries, Bo Shen has been actively involved in Bitcoin, Ripple and BitShares. 
 					He is currently the one of three founding members of the Bitcoin Foundation's China Chapter, the Director of MyBitfund, Invictus Innovations and RippleChina.  
-				</p>
+				</p>-->
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Li Xiaolai - Bio Card.png" alt="" /></a>
-				<p>	Li Xiaolai<br>Director </p>
+				<!--<p>	Li Xiaolai<br>Director </p>
 				<p style="text-align: justify;">
 					Li Xiaolai is an entrepreneur, author, social innovator and educator based in Beijing. Author of the best-selling book Befriending Time.
 					His work at the New Oriental School gave him recognition as one of the best teachers in China. 
 					Xiaolai is the founder of several ventures including the website knewone.com, the venture capital fundbitfund.pe and 
 					his own chain of private schools in Beijing,Shanghai,Shenzhen and Nanjing. He supports bitcointalk.org as a VIP member and as a lifetime member of the Bitcoin Foundation. 
-				</p>
+				</p>-->
 			</div>
 		</div><!-- /row -->
 		<div class="row mt centered">	
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Stan Larimer - Bio Card.png" alt="" /></a>
-				<p>	Stan Larimer<br>President, Chief Operating Officer  </p>
+				<!--<p>	Stan Larimer<br>President, Chief Operating Officer  </p>
 				<p style="text-align: justify;">
 					Stan Larimer is a senior program manager and systems/hardware/ software engineer with 37 years experience with unmanned systems in the aerospace and internet industries. 
 					He has served as an Assistant Vice President for Science Applications International Corporation (SAIC) and has worked with and for 
 					the Air Force Wright Patterson Labs and many of the top companies in the American aerospace industry, He also taught rocket science at the U.S.Air Force Academy.  
-				</p>
+				</p>-->
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Brian Page Bio Card.png" alt="" /></a>
-				<p>	Brian Page<br>Director of Marketing </p>
+				<!--<p>	Brian Page<br>Director of Marketing </p>
 				<p style="text-align: justify;">
 					Brian Page is a trends investor, entrepreneur and marketing specialist with a passion for building the next great brand. 
 					His primary talent is the ability to craft a simple message that's relevant, consistent and recognizable. 
 					He takes pride in making the complex easy to understand and championing the revolutionary concepts of crypto-currencies, crypto-equities and decentralization. 
 					He's thrilled to be joining the Invictus team as its brand champion. 
-				</p>
+				</p>-->
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Charles Evans - Bio Card.png" alt="" /></a>
-				<p>	Dr. Charles Evans<br>Economic Consultant </p>
+				<!--<p>	Dr. Charles Evans<br>Economic Consultant </p>
 				<p style="text-align: justify;">
 					Teaches Finance and Economics, both on-line and in the classroom and has spent the past two decades working on the Internet in several capacities.
 					Served as financial cryptographic consultant on several online payments system startups during the height of the Dot.Com Era. Dean of Online Business Programs for Keiser University in Florida.
 					Currently developing economic underpinnings for Invictus Innovations while teaching on-line and building Pecuniology.corn, which is dedicated to promoting economic and financial literacy worldwide. 
-				</p>
+				</p>-->
 			</div>
 			<div class="row mt centered">	
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Pam Larimer - Bio Card.png" alt="" /></a>
-				<p>	Pam Larimer<br>Board Secretary Business Administrator   </p>
+				<!--<p>	Pam Larimer<br>Board Secretary Business Administrator   </p>
 				<p style="text-align: justify;">
 					Pam Larimer has 30 years experience as Office Manager for Booster Products in Ohio, is administrator and executive assistant for the Florida Public Schools and Admissions Director and 
 					Assistant to the Headmaster at Loudoun Country Day School, an elite private school in Virginia  
-				</p>
+				</p>-->
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Dan Notestein - Bio Card.png" alt="" /></a>
-				<p>	Dan Notestein<br>Contractor, Consultant, Senior S/W Engineer  </p>
+				<!--<p>	Dan Notestein<br>Contractor, Consultant, Senior S/W Engineer  </p>
 				<p style="text-align: justify;">
 					Dan Notestein's industry experience includes 14 years of hardware and software development. 
 					At Texas Instruments he worked as both a software design engineer and a hardware design engineer. 
 					Notestein also worked at Burr Brown Corporation, where he was project leader for the design of a custom VXI-based test system for high-performance analog ICs.
 					In addition to his system level work, Notestein was also responsible for designing several high-speed data acquisition and control boards that operated in the system. He is now a free-lance developer. 
-				</p>
+				</p>-->
 			</div>
 			<div class="col-lg-6 desc">
 				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/team/Omar Asor - Bio CArd.png" alt="" /></a>
-				<p>	Ohad Asor<br>Developer  </p>
+				<!--<p>	Ohad Asor<br>Developer  </p>
 				<p style="text-align: justify;">
 					Ohad Asor is a Mathematician and a Software Engineer, experienced at the top Israeli software companies since 1995.
 					C. and Linux expert. He is also a researcher at the field of Machine Learning Theory.
 					In addition, studied psychology, physics and philosophy, and has a great deal with technological innovations. 
 					Ohad strongly believes in justice for all, and is an enthusiastic member of the Open Source and crypto-currency communities. 
-
-				</p>
+				</p>-->
 			</div>
 		</div><!-- /row -->
 
