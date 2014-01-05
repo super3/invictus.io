27073f310e277ec85461fcc8052f03830ce58534
super3 <me@super3.org>
2014-01-05 17:45:16 -0500

    New Angelshare Page

--- funding-angelshares.php
+++ funding-angelshares.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
-
+	
 	<! ========== FULL PROJECT WRAP ============================================================================================= 
 	=============================================================================================================================>    
 	<div id="angel-big">
@@ -7,13 +7,16 @@
 		</div><!-- /container -->
 	</div><!-- /fullproject -->
 
-	<! ========== FEATURED WORDS ================================================================================================ 
+	<! ========== Content   	 ================================================================================================ 
 	=============================================================================================================================>    
-    <div class="container" style="margin-bottom: 0px;">
-    <div class="inner-page row">
-    	<div class="row">
-		<div class="col-lg-7">
-			<h3>What are Angelshares?</h3>					
+
+	<div id="white">
+		<div class="container">			
+			<div class="inner-page row">
+			
+				<div class="row">				
+					<div class="col-lg-7">					
+							<h3>What are Angelshares?</h3>					
 			<p style="text-align: justify;">								
 				AngelShares, like ProtoShares, represent a way to gain a stake in the new crypto-equity industry.  
 				ProtoShares acknowledge a contribution to the launch of the new industry, representing a proof
@@ -52,16 +55,12 @@
 				<li><a href="#">Social Consensus - PDF file of the social contract between Invictus Innovations and investors</a></li>
 				</ul>
 					</p>	
-			
-		</div><!-- /col-lg-4 -->
-		
-	
+								
+					</div>				
 
-    	</div><!-- /row -->
-
-    	<div class="row">
+					<div class="row">
 						<div class="col-lg-5">
-								<h3>How to acquire Angelshares?</h3>					
+							<h3>How to acquire Angelshares?</h3>					
 			<p style="text-align: justify;">								
 				Beginning New Year’s Day 2014, there will be 10,000 new AngelShares available in a new patron competition, every day, for 200 days. 
 				5000 of them will be available to ProtoShare holders. 5000 of them will be available to Bitcoin holders. That's ultimately 2,000,000 total shares, just like ProtoShares.
@@ -80,34 +79,17 @@
 			<p>
 				Every day the shares are divided among those who donated that day.
 			</p>
+						</div>
 
-		</div>
-	</div> </div>
-    </div><!-- /container -->
-
-	<! ========== Content   	 ================================================================================================ 
-	=============================================================================================================================>    
-
-	<div id="white">
-		<div class="container">			
-			<div class="inner-page row">
-			
-				<div class="row">				
-					<div class="col-lg-12">					
-							
-					
-					
 					</div>		
-
 				</div>	
-				<br><br>
 
 			</div> 		
  	
 
     	</div><!-- /row -->
 			
-		</div><!-- /container -->
+		
 	</div><!-- /white -->
 
 <?php include 'footer.php'; ?>
