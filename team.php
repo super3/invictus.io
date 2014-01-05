c7701854c4bfeba1edb4df71772f9f860da91124
super3 <me@super3.org>
2014-01-05 18:27:01 -0500

    Added Bio Cards

--- team.php
+++ team.php
@@ -26,37 +26,94 @@
 			</div>
 		</div><!-- /row -->
 		<div class="row mt centered">	
-			<div class="row sqs-row" id="yui_3_10_1_1_1388964224483_125"><div class="col sqs-col-6 span-6" id="yui_3_10_1_1_1388964224483_124"><div class="sqs-block image-block sqs-block-image" data-block-type="5" id="block-091242d6f2b5ff4e7728"><div class="sqs-block-content" id="yui_3_10_1_1_1388964224483_123">
-	<div class="image-block-outer-wrapper layout-caption-below " id="yui_3_10_1_1_1388964224483_122">
-	
-		<div class="intrinsic" style="max-width:525.0px;" id="yui_3_10_1_1_1388964224483_121">
-			
-				<div style="padding-bottom:75.61904907226562%;" class="image-block-wrapper   " data-description="" id="yui_3_10_1_1_1388964224483_120">
-					<noscript>&lt;img src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc2b0e4b0c0e9b155a65e/1383973554117/Daniel%20Larimer%20-%20Bio%20Card.png"  alt="Daniel Larimer - Bio Card.png"  /&gt;</noscript><img class="thumb-image loaded" data-src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc2b0e4b0c0e9b155a65e/1383973554117/Daniel%20Larimer%20-%20Bio%20Card.png" data-image="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc2b0e4b0c0e9b155a65e/1383973554117/Daniel%20Larimer%20-%20Bio%20Card.png" data-image-dimensions="525x397" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="527dc2b0e4b0c0e9b155a65e" data-type="image" style="" alt="Daniel Larimer - Bio Card.png" src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc2b0e4b0c0e9b155a65e/1383973554117/Daniel%20Larimer%20-%20Bio%20Card.png?format=500w" data-image-resolution="500w">
-				</div>
-			
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Daniel Larimer <br>CEO America, CTO, Founder, Chairman </p>
+				<p style="text-align: justify;">
+					Daniel Larimer is a Virginia based entrepreneur and software engineer with a passion to find free market solutions to secure life, liberty and property for all. 
+					In addition to software development, Daniel is a self-taught expert in Austrian economics. He has recently co-founded Invictus Innovations Incorporated with the mission of 
+					developing decentralized solutions to centralized problems where he serves as the CTO and is the chief architect of the BitShares peer-to-peer trust-less, 
+					black-chain-based,financial system. 
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Bo Shen<br>Director, CEO China </p>
+				<p style="text-align: justify;">
+					Bo Shen is an evangelist, entrepreneur and inverter in math based ecosystem. As a veteran of the financial and Internet industries, Bo Shen has been actively involved in Bitcoin, Ripple and BitShares. 
+					He is currently the one of three founding members of the Bitcoin Foundation's China Chapter, the Director of MyBitfund, Invictus Innovations and RippleChina.  
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Li Xiaolai<br>Director </p>
+				<p style="text-align: justify;">
+					Li Xiaolai is an entrepreneur, author, social innovator and educator based in Beijing. Author of the best-selling book Befriending Time.
+					His work at the New Oriental School gave him recognition as one of the best teachers in China. 
+					Xiaolai is the founder of several ventures including the website knewone.com, the venture capital fundbitfund.pe and 
+					his own chain of private schools in Beijing,Shanghai,Shenzhen and Nanjing. He supports bitcointalk.org as a VIP member and as a lifetime member of the Bitcoin Foundation. 
+				</p>
+			</div>
+		</div><!-- /row -->
+		<div class="row mt centered">	
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Stan Larimer<br>President, Chief Operating Officer  </p>
+				<p style="text-align: justify;">
+					Stan Larimer is a senior program manager and systems/hardware/ software engineer with 37 years experience with unmanned systems in the aerospace and internet industries. 
+					He has served as an Assistant Vice President for Science Applications International Corporation (SAIC) and has worked with and for 
+					the Air Force Wright Patterson Labs and many of the top companies in the American aerospace industry, He also taught rocket science at the U.S.Air Force Academy.  
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Brian Page<br>Director of Marketing </p>
+				<p style="text-align: justify;">
+					Brian Page is a trends investor, entrepreneur and marketing specialist with a passion for building the next great brand. 
+					His primary talent is the ability to craft a simple message that's relevant, consistent and recognizable. 
+					He takes pride in making the complex easy to understand and championing the revolutionary concepts of crypto-currencies, crypto-equities and decentralization. 
+					He's thrilled to be joining the Invictus team as its brand champion. 
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Dr. Charles Evans<br>Economic Consultant </p>
+				<p style="text-align: justify;">
+					Teaches Finance and Economics, both on-line and in the classroom and has spent the past two decades working on the Internet in several capacities.
+					Served as financial cryptographic consultant on several online payments system startups during the height of the Dot.Com Era. Dean of Online Business Programs for Keiser University in Florida.
+					Currently developing economic underpinnings for Invictus Innovations while teaching on-line and building Pecuniology.corn, which is dedicated to promoting economic and financial literacy worldwide. 
+				</p>
+			</div>
+			<div class="row mt centered">	
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Pam Larimer<br>Board Secretary Business Administrator   </p>
+				<p style="text-align: justify;">
+					Pam Larimer has 30 years experience as Office Manager for Booster Products in Ohio, is administrator and executive assistant for the Florida Public Schools and Admissions Director and 
+					Assistant to the Headmaster at Loudoun Country Day School, an elite private school in Virginia  
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Dan Notestein<br>Contractor, Consultant, Senior S/W Engineer  </p>
+				<p style="text-align: justify;">
+					Dan Notestein's industry experience includes 14 years of hardware and software development. 
+					At Texas Instruments he worked as both a software design engineer and a hardware design engineer. 
+					Notestein also worked at Burr Brown Corporation, where he was project leader for the design of a custom VXI-based test system for high-performance analog ICs.
+					In addition to his system level work, Notestein was also responsible for designing several high-speed data acquisition and control boards that operated in the system. He is now a free-lance developer. 
+				</p>
+			</div>
+			<div class="col-lg-4 desc">
+				<a class="zoom blue" href="#"><img class="img-responsive" src="assets/img/bitcoin.png" alt="" /></a>
+				<p>	Ohad Asor<br>Developer  </p>
+				<p style="text-align: justify;">
+					Ohad Asor is a Mathematician and a Software Engineer, experienced at the top Israeli software companies since 1995.
+					C. and Linux expert. He is also a researcher at the field of Machine Learning Theory.
+					In addition, studied psychology, physics and philosophy, and has a great deal with technological innovations. 
+					Ohad strongly believes in justice for all, and is an enthusiastic member of the Open Source and crypto-currency communities. 
 
-			
-
-		</div>
-	
-	</div>
-</div></div></div><div class="col sqs-col-6 span-6" id="yui_3_10_1_1_1388964224483_142"><div class="sqs-block image-block sqs-block-image" data-block-type="5" id="block-0fd14048e227e58be20a"><div class="sqs-block-content" id="yui_3_10_1_1_1388964224483_141">
-	<div class="image-block-outer-wrapper layout-caption-below " id="yui_3_10_1_1_1388964224483_140">
-	
-		<div class="intrinsic" style="max-width:525.0px;" id="yui_3_10_1_1_1388964224483_139">
-			
-				<div style="padding-bottom:75.61904907226562%;" class="image-block-wrapper   " data-description="" id="yui_3_10_1_1_1388964224483_138">
-					<noscript>&lt;img src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc36de4b0bfa86bc0a3b6/1383973743548/Bo%20Shen%20-%20Bio%20Card.png"  alt="Bo Shen - Bio Card.png"  /&gt;</noscript><img class="thumb-image loaded" data-src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc36de4b0bfa86bc0a3b6/1383973743548/Bo%20Shen%20-%20Bio%20Card.png" data-image="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc36de4b0bfa86bc0a3b6/1383973743548/Bo%20Shen%20-%20Bio%20Card.png" data-image-dimensions="525x397" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="527dc36de4b0bfa86bc0a3b6" data-type="image" style="" alt="Bo Shen - Bio Card.png" src="http://static.squarespace.com/static/51fb043ee4b0608e46483caf/t/527dc36de4b0bfa86bc0a3b6/1383973743548/Bo%20Shen%20-%20Bio%20Card.png?format=500w" data-image-resolution="500w" id="yui_3_10_1_1_1388964224483_349">
-				</div>
-			
-
-			
-
-		</div>
-	
-	</div>
-</div></div></div></div>
+				</p>
+			</div>
 		</div><!-- /row -->
 
 	</div><!-- /container -->	
