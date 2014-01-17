94671918fe1a5257979a6346f14964c7f6787524
super3 <me@super3.org>
2014-01-17 00:26:21 -0500

    Removed Protoshares Branding

--- funding-protoshares.php
+++ funding-protoshares.php
@@ -19,24 +19,24 @@
 			
 				<div class="row">				
 					<div class="col-lg-7">					
-						<h3>What are ProtoShares?</h3>					
+						<h3>What are Bitshares PTS?</h3>					
 							<p style="text-align: justify;">								
-								ProtoShares is a class of crypto-currency, based of Bitcoin, that lets you capture a position in one or more new products before
-								they are even implemented. It is also a fund raising mechanism for Bitshares. The speculative value of a DACs ProtoShares is a subjective mix of three market
+								Bitshares PTS is a class of crypto-currency, based of Bitcoin, that lets you capture a position in one or more new products before
+								they are even implemented. It is also a fund raising mechanism for Bitshares. The speculative value of a DACs Bitshares PTS is a subjective mix of three market
 								perception pillars. These pillars are the market’s assessment of:
 							</p>
 							<ul style="list-style-type: circle;">
 								<li>The speculative value of a future product or DAC share.</li>
 								<li>The probability that the product or DAC can and will be successfully fielded.</li>
-								<li>Confidence that the ProtoShares map to an equitable share in the implementation.</li>
+								<li>Confidence that the Bitshares PTS map to an equitable share in the implementation.</li>
 							</ul> <br>
 							<p style="text-align: justify;">
-								Developers use ProtoShares to raise capital with which to implement a new idea. They are a new
+								Developers use Bitshares PTS to raise capital with which to implement a new idea. They are a new
 								decentralized form of crowd sourcing using incorruptible block chain technology and social contracts in lieu of lawyers,
 								bankers and regulators to fund and launch a start-up company.
 							</p>
 							<p style="text-align: justify;">
-								Investors use ProtoShares to get in on the ground floor of an idea they recognize as having potential.
+								Investors use Bitshares PTS to get in on the ground floor of an idea they recognize as having potential.
 								They continue to trade them to motivate vetting of the idea in public debate forums and to increase the
 								likelihood that other investors and developers will get involved in making the idea a reality. As the
 								public vetting unfolds, everyone can compete using their own wisdom to decide whether they want to
