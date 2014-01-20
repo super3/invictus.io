82dba215c839e5e89c344b7dacefd81beed3bf9d
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-01-20 16:58:18 -0500

    adding protoshares download link

--- bitshares-pts.php
+++ bitshares-pts.php
@@ -31,6 +31,9 @@
               Invictus has chosen to allocate 50% to PTS holders in the genesis block.
               </p>
               <p>
+              <a class="btn btn-primary btn-lg" href="http://protoshares.net/index.html#download">Download Wallet</a>
+              </p>
+              <p>
               We have previously referred to BitShares PTS as "ProtoShares", but no longer do so.  "ProtoShare" is a registered trademark of Site9, Inc. for its prototyping and mockup software product, ProtoShare.  There is no relationship between ProtoShare and Invictus Innovations.
               </p>
 					</div>				
