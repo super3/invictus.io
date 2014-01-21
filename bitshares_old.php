c38a74f070c78785e10d0de5576b35b41df41020
Justin van Oel <jvanoel@gmail.com>
2014-01-21 21:55:17 +0100

    Add BitShares Page and fixes Index

---
+++ bitshares_old.php
@@ -0,0 +1,71 @@
+<?php include 'header.php'; ?>
+
+	<! ========== BLACK SECTION ================================================================================================= 
+	=============================================================================================================================>    
+	<div id="bitshare-big">
+		<div class="container">
+		</div><!-- /container -->
+	</div><!-- /fullproject -->
+
+	<! ========== Content   	 ================================================================================================ 
+	=============================================================================================================================>    
+
+	<div id="white">
+		<div class="container">			
+			<div class="inner-page row">			
+				<div class="row">				
+					<div class="col-lg-9" style="margin-bottom: -25px;">				
+						<h3>What are Bitshares?</h3>					
+							<p style="text-align: justify;">								
+								BitShares (BTS) is a revolutionary new product for the cryptocurrency world. It will combine all the things that are advantageous about cryptocurrencies, the open format, the security and the non-controlable nature of it, and combine it
+								with the possibilities that the last decades of economic innovation have brought us. This will be the combination of the best of two worlds and will be a major step towards general cryptocurrency acceptance. 
+							</p>
+							<p style="text-align: justify;">
+								We as Invictus Innovations acknowledge that the base of an economy is not the currencies we have available but the possibilities and freedom we have for trading. The way implement his idea is that BitShares (BTS) offers a bank account that earns 5% interest where funds can be transferred in minutes anywhere in the world with more privacy and security than a Swiss bank account.  
+								Your account can never be frozen, your funds cannot be seized, and the bank can never face collapse due to loan defaults or fraud.  All of this is made possible without requiring any employees, lawyers, regulatory compliance, vaults, buildings, and other infrastructure required by traditional banks.  
+								And it will allow you to do anything with your funds, both fiat and digital, that you can do in the fiat world, from investments to selling packaged financial products.
+							</p>
+							<p style="text-align: justify;">
+								Unlike existing banks, BitShares is currency agnostic, you can hold your balance denominated in gold, silver, oil, or other commodities in additional to national currencies while earning 5% interest. 
+							    BitShares can achieve this impressive feat using the same technology that makes Bitcoin possible, irrevokable decentralized automated consensus forming.     
+							</p>
+							<h3>For consumers</h3>
+							<p style="text-align: justify;">
+								Bitshares will allow you to enjoy what cryptocurrencies offer, a safe, private, fast, irrepressible payments around the globe. Furthermore Bitshares will allow you to leverage your funds, being fiat or equity, in a 
+								similar, distributed fashion. The innovations that Bitcoin brought to fiat currencies will be equivalent to the innovations that Bitshares will bring to equity and currency trading. 
+							</p>								
+							
+							<h3>For investors</h3>			
+							<p style="text-align: justify;">
+								Investing in cryptocurriencies was already seen as lucrative bu smaller parties but slowly the market for these currencies is growing and larger institutions are taking part. But Bitcoin is not as good an investment as it looks. You can view Bitcoin as stock in a company that earns no profits, pays 100% of its revenue to security guards, and then debases shareholders by 12% per year to fund security. 
+								Despite the inefficiencies and costs, Bitcoin has grown to be worth over $12 billion dollars in less than 5 years.  How much more valuable would Bitcoin be if it could turn a profit while offering far more powerful services? 
+								The value of owning shares in this new Bank will exceed two times the value of all balances of in gold, silver, oil, and currency accounts.  If the largest banks can achieve deposits of over $1 trillion dollars with no meaningful interest, how many deposits could BitShares attract and what would that mean for the value of the bank?
+								
+								Return on investment is generated for investors in Bitshares by taking a cut on every transaction and paying 100% of these transaction fees as dividends. Trading of shares in Bitshares will be possible within the system itself. 
+							</p>
+							<p style="text-align: justify;">				
+								If you would like to get in on the ground floor of such a revolutionary decentralized business you can get involved by contributing your time, money, and services as part of the growing movement at <a href="http://bitsharestalk.org">bitsharestalk.org</a>. See the resources below for more information.
+							</p>				
+					</div>
+					<div class="col-lg-3" style="padding-top: 60px;">
+						<img src="assets/img/stock-market.jpg" width="400px">
+						
+						<h3>Resources</h3>	
+							<p>	
+								<ul style="list-style-type: circle;">
+									<li><a href="https://bitsharestalk.org/index.php?board=3.0">Bitshares discussion board <br> Bitsharetalk.org forum</a></li>
+									<li><a href="https://github.com/InvictusInnovations/BitShares">Bitshares Github <br> Source and development information</a></li>
+									<li><a href="https://www.youtube.com/watch?v=5BV55IrZi7g&html5=1&hd=1">Bitshares Introductory Video</a></li>
+								</ul>
+							</p>	
+					</div>
+				</div>
+			
+			</div>	
+				<br><br>
+
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /white -->
+
+<?php include 'footer.php'; ?>
