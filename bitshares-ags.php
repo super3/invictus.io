758d34683431f62f230084e9e228cbaaa7d491b4
Phoenix <phoenix@todofixthis.com>
2014-01-28 00:31:55 -0300

    Fixed pluralization in bitshares-ags.php
    "all derivative software products to initialize their blockchain" is ambiguous; it is unclear from the wording whether all derivative software products share a single blockchain, or if each one has its own.

--- bitshares-ags.php
+++ bitshares-ags.php
@@ -88,7 +88,7 @@
 							</ul>
               <br/>
               This emerging social consensus and resulting network effect will be further reinforced by the Social Consensus Software License
-              which will require all derivative software products to initialize their blockchain according to this consensus.  
+              which will require each derivative software products to initialize its blockchain according to this consensus.  
               <br/>
               <br/>
               <br/>
