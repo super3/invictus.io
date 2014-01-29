72c227d8f3b3bc3cd40c4244f6d11a5531259698
currencydebt <contact@currencydebt.com>
2014-01-28 21:30:08 -0800

    added author and description in meta tags
    It appears the header file is used on all pages, so having a unique meta description tag for each page is not possible as currently constructed. The description meta tag should be longer, but if it is identical on every page than that defeats the purpose of describing what is on each unique page

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Bitshares">
+    <meta name="author" content="Invictus">
     <link rel="shortcut icon" href="assets/ico/favicon.png">
 
     <title>Invictus Innovations - Reimagine Everything</title>
